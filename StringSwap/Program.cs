91561cd503c8c842b4b5dd4b017ebb9717860d79
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 20:12:36 +0200

    first and final commit

---
+++ StringSwap/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringSwap
+{
+    
+    class Program
+    {
+        static string swapStrings(string s1, string s2)
+        {
+            s1 = s1 + s2;
+            s2 = s1.Substring(0, s1.Length - s2.Length);
+            s1 = s1.Substring(s2.Length);
+            return "s1 is now " + s1 + " " + "s2 is now " + s2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(swapStrings("eddie", "mandx"));
+            
+            
+        }
+    }
+}
