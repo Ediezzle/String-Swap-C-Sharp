44eca2561041dc4a6e3e140d4fe08f70a7b1943d
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-30 04:14:24 +0200

    add comment with alternative way to obtain the other string (s1 or first string)

--- StringSwap/Program.cs
+++ StringSwap/Program.cs
@@ -9,15 +9,16 @@ namespace StringSwap
         {
             s1 = s1 + s2;
             s2 = s1.Substring(0, s1.Length - s2.Length);
-            s1 = s1.Substring(s2.Length);
+            s1 = s1.Substring(s2.Length); //or s1 = s1.Substring(s2.Length, s1.Length - s2.Length);
             return "s1 is now " + s1 + " " + "s2 is now " + s2;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(swapStrings("eddie", "mandx"));
-            
-            
+            Console.WriteLine(swapStrings("hey", "you"));
+
+
         }
     }
 }
